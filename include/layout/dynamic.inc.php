9741c98bb59e5c4fc62c4a60d77a144cde4b7836
Your Name <you@example.com>
2015-09-19 08:19:21 -0400

    Test

--- include/layout/dynamic.inc.php
+++ include/layout/dynamic.inc.php
@@ -4,7 +4,8 @@ function dynamic_menu_content() {
     $cache_name = user_is_logged_in() ? 'logged_in_'.$_SESSION['class'] : 'guest';
 
     if (cache_start($cache_name, CONFIG_CACHE_TIME_DYNAMIC, CONST_CACHE_DYNAMIC_MENU_GROUP)) {
-        $entries = db_query_fetch_all(
+        $values[':class'] = $_SESSION['class'];
+	$entries = db_query_fetch_all(
             'SELECT
                 title,
                 internal_page,
@@ -15,10 +16,10 @@ function dynamic_menu_content() {
                 dynamic_menu
             WHERE
                 '.(user_is_logged_in() ?
-                    'min_user_class <= '.$_SESSION['class'].' AND (visibility = "private" OR visibility = "both")' :
+                    'min_user_class <= :class AND (visibility = "private" OR visibility = "both")' :
                     'visibility = "public" OR visibility = "both"'
             ).'
-            ORDER BY priority DESC'
+            ORDER BY priority DESC', $values
         );
 
         foreach($entries as $entry) {
